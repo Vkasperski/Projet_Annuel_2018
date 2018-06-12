53bc89d0d19e29ff24aca83c2cae94761e041459
Vkasperski <victor.kasperski@laposte.net>
2018-06-12 09:51:11 +0200

    update

--- Appli_Gest_Room/projet/vues/v_moncompte.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid">
-    <!-- Breadcrumbs-->
-    <ol class="breadcrumb">
-    	<li class="breadcrumb-item">
-          <a href="#">Dashboard</a>
-        </li>
-        <li class="breadcrumb-item active">Réservations</li>
-     </ol>
-     </div>
